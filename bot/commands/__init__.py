20466368c8b6d783fe78b0bae1c8a2bdffab5354
DmitriySudd <dimsud1@gmail.com>
2024-03-10 15:58:02 +0400

    Your Message

--- bot/commands/__init__.py
+++
@@ -1,9 +0,0 @@
-__all__ = ['cmd_start', 'cmd_quiz', 'new_quiz', 'update_quiz_index']
-
-from bot.commands.commands import (
-    cmd_start, cmd_quiz, new_quiz,
-)
-from bot.commands.table import create_table, update_quiz_index
-
-
-
